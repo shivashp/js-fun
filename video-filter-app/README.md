e9aafdcc163e9aac76cc2386f124763d260bb4b8
shivashp <shivapandey04@gmail.com>
2017-11-04 14:13:37 +0545

    Restructure

---
+++ video-filter-app/README.md
@@ -0,0 +1,1 @@
+# Small Fun Projects in Javascript
---
+++ video-filter-app/index.js
@@ -0,0 +1,21 @@
+const express = require('express');
+const app = express();
+const fs = require('fs');
+const https = require('https');
+
+// SSL certificate to use https
+const sslOptions = {
+    key: fs.readFileSync('key.pem', 'utf8'),
+    cert: fs.readFileSync('cert.pem', 'utf8'),
+    passphrase: 'shiva'
+  };
+  
+const server = https.createServer(sslOptions, app)
+
+app.use(express.static(__dirname + '/public')); 
+
+app.get('/', function (req, res) {
+  res.sendfile(__dirname + '/public/index.html');
+});
+
+server.listen(8080);
